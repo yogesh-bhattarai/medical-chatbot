87f03400c574af4f96c01262006b55f591365e05
Yogesh <yogesh.bhattarai633@gmail.com>
2025-03-21 23:52:33 +0545

    qdrant successfully done

--- ingest.py
+++ ingest.py
@@ -7,22 +7,27 @@ from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Qdrant
 from langchain_community.vectorstores import Pinecone
 from dotenv import load_dotenv
-from langchain_pinecone import PineconeVectorStore
+# from langchain_pinecone import PineconeVectorStore
 
 load_dotenv()
 
 embeddings= SentenceTransformerEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
-path='./Data'
-loader= DirectoryLoader(path ,glob='*.pdf',show_progress=True,loader_cls=PyPDFLoader)
+
+data_path = os.path.join(os.path.dirname(__file__), "Data")
+
+
+loader= DirectoryLoader(data_path, glob='*.pdf',show_progress=True,loader_cls=PyPDFLoader)
 documents= loader.load()
 
 text_splitter= RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
 texts= text_splitter.split_documents(documents)
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-index_name= "medical-chatbot"
-docsearch = PineconeVectorStore.from_documents(
-    documents=texts,
-    index_name='medical-chatbot',
-    embedding=embeddings,
+url = "http://localhost:6333"
+qdrant = Qdrant.from_documents(
+    texts,
+    embeddings,
+    url=url,
+    prefer_grpc=False,
+    collection_name="vector_db"
 )
+
+print("Vector DB Successfully Created!")
