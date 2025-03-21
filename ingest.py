9ec2d0ebddc525ab6ca1d02556dc9db0f4cabb39
Yogesh <yogesh.bhattarai633@gmail.com>
2025-03-21 22:56:15 +0545

    pinecone_setup

--- ingest.py
+++ ingest.py
@@ -5,10 +5,24 @@ from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.vectorstores import Qdrant
+from langchain_community.vectorstores import Pinecone
+from dotenv import load_dotenv
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
 
 embeddings= SentenceTransformerEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
 path='./Data'
-loader= DirectoryLoader(path,glob='*.pdf',show_progress=True,loader_cls=PyPDFLoader)
+loader= DirectoryLoader(path ,glob='*.pdf',show_progress=True,loader_cls=PyPDFLoader)
 documents= loader.load()
 
-print(len(documents))
+text_splitter= RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+texts= text_splitter.split_documents(documents)
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+index_name= "medical-chatbot"
+docsearch = PineconeVectorStore.from_documents(
+    documents=texts,
+    index_name='medical-chatbot',
+    embedding=embeddings,
+)
