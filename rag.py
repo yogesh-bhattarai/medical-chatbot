0e7d812981d6fb8709fc1b579c8336f0e060f759
Yogesh <yogesh.bhattarai633@gmail.com>
2025-03-23 01:22:49 +0545

    yet to finished implemented llm

--- rag.py
+++ rag.py
@@ -1,8 +1,8 @@
-from langchain_core.prompts import PromptTemplate
-from langchain_community.llms import ctransformers
-from langchain.chains import retrieval_qa
+from langchain import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
 from langchain_community.embeddings import SentenceTransformerEmbeddings
-from fastapi import FastAPI, Request,Form,Response
+from fastapi import FastAPI, Request, Form, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -11,61 +11,58 @@ from qdrant_client import QdrantClient
 from langchain_community.vectorstores import Qdrant
 import os
 import json
-app= FastAPI()
-templates=  Jinja2Templates(directory= "templates")
 
-#app.mount("/static", StaticFiles(directory="static"),name="static")
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# config= {
-#     "max_new_token":1024,
-#     "context_lenght":2048,
-#     "repetition_penalty":1.1,
-#     "temperature": 0.1,
-#     "top_k":50,
-#     "top_p":0.9,
-#     "stream": True,
-#     "threads": int(os.cpu_count()/2) 
-# }
-# model= AutoModelForCausalLM.from_pretrained(
-#     "mradermacher/Meditron3-Gemma2-2B-i1-GGUF",
-#     model_type="llama",
-#     lib="avx2",
-# )
-from transformers import AutoModelForCausalLM,AutoTokenizer
-model_name= "openai-community/gpt2"
-tokenizer= AutoTokenizer.from_pretrained(model_name,trust_remote_code= True)
-model= AutoModelForCausalLM.from_pretrained(model_name)
+
+llm =CTransformers(
+    model="TheBloke/Llama-2-7B-Chat-GGML",
+    model_type="llama",
+    max_new_tokens=512,
+    temperature=0.7)
 
 print("LLM Initialized....")
 
-prompt_template= """
-Use the following pieces of information to answer  the user's question 
-If you donot know the answer, just say that  you don't know don't try to make up an answer.
+prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-Context:{context}
-Question:{question}
+Context: {context}
+Question: {question}
 
-only return the helpful answer below  and nothing else
+Only return the helpful answer below and nothing else.
 Helpful answer:
-
 """
 
-embeddings= SentenceTransformerEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
+embeddings = SentenceTransformerEmbeddings(model_name="NeuML/pubmedbert-base-embeddings")
 
-url= "http://localhost:6333"
-client= QdrantClient(
-    url=url,
-     prefer_grpc=False)
-db= Qdrant(client=client,embeddings=embeddings,collection_name="vector_db")
-prompt= PromptTemplate(template=prompt_template,input_variables=["context","question"])
-retrieval= db.as_retriever(search_kwargs= {"k":1})
+url = "http://localhost:6333"
 
-@app.get("/",response_class=HTMLResponse)
-async def read_root(request:Request):
-    return templates.TemplateResponse("index.html",{"request":request})
+client = QdrantClient(
+    url=url, prefer_grpc=False
+)
 
-#@app.post("/get_response")
+db = Qdrant(client=client, embeddings=embeddings, collection_name="vector_db")
 
- 
+prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
 
+retriever = db.as_retriever(search_kwargs={"k":1})
 
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/get_response")
+async def get_response(query: str = Form(...)):
+    chain_type_kwargs = {"prompt": prompt}
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True, chain_type_kwargs=chain_type_kwargs, verbose=True)
+    response = qa(query)
+    print(response)
+    answer = response['result']
+    source_document = response['source_documents'][0].page_content
+    doc = response['source_documents'][0].metadata['source']
+    response_data = jsonable_encoder(json.dumps({"answer": answer, "source_document": source_document, "doc": doc}))
+    
+    res = Response(response_data)
+    return res
